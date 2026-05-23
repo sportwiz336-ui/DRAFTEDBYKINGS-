43c73037094e90bb437cbc30acf6eeb7c3d1a804
Colin Meredith <sportwiz336@gmail.com>
2026-05-23 00:18:27 +0000

    Initial commit

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.ace_app_fresh"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        applicationId = "com.example.ace_app_fresh"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
---
+++ android/temp_android_base/android/app/src/main/kotlin/com/example/temp_android_base/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.temp_android_base
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
